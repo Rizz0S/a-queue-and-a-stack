b169e8470a13e871d056d02dbff712d3ea33c78e
Summer Rizzo <39225869+Rizz0S@users.noreply.github.com>
2020-06-24 16:12:42 -0400

    add queue

---
+++ queue.js
@@ -0,0 +1,12 @@
+var Queue = function() {
+    this.items = [];
+};
+Queue.prototype.enqueue = function(obj) {
+    this.items.push(obj);
+};
+Queue.prototype.dequeue = function() {
+    return this.items.shift();
+};
+Queue.prototype.isEmpty = function() {
+    return this.items.length === 0;
+};
